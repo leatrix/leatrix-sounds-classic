4810e148bbe30a155c452eb77bb95a234f39fc13
leatrix <58888415+leatrix@users.noreply.github.com>
2021-06-09 12:39:41 +0100

    1.13.109

--- Leatrix_Sounds.lua
+++ Leatrix_Sounds.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- Leatrix Sounds 1.13.105 (13th May 2021)
+	-- Leatrix Sounds 1.13.109 (9th June 2021)
 	----------------------------------------------------------------------
 
 	--  Create global table
@@ -10,7 +10,7 @@
 	local LeaSoundsLC, LeaSoundsCB, LeaDropList = {}, {}, {}
 
 	-- Version
-	LeaSoundsLC["AddonVer"] = "1.13.105"
+	LeaSoundsLC["AddonVer"] = "1.13.109"
 	LeaSoundsLC["RestartReq"] = nil
 
 	-- Get locale table
--- Leatrix_Sounds_Files.lua
+++ Leatrix_Sounds_Files.lua
@@ -1,13 +1,13 @@
 
 	----------------------------------------------------------------------
 	-- Leatrix Sounds Sound File Listing
-	-- Community CSV for wow_classic 1.13.7.38631 (ClassicRetail)
+	-- Community CSV for wow_classic_era 1.13.7.38704 (ClassicRetail)
 	----------------------------------------------------------------------
 
 	local void, Leatrix_Sounds = ...
 	local L = Leatrix_Sounds.L
 
-	Leatrix_Sounds["ListFileVersion"] = "1.13.7.38631"
+	Leatrix_Sounds["ListFileVersion"] = "1.13.7.38704"
 
 	Leatrix_Sounds["MP3"] = {
 
