d443f669e2894ff39cb113e9b31b53f465d88d1d
leatrix <58888415+leatrix@users.noreply.github.com>
2021-11-24 03:21:36 +0000

    1.14.09

--- Leatrix_Sounds_Files.lua
+++ Leatrix_Sounds_Files.lua
@@ -1,13 +1,13 @@
 
 	----------------------------------------------------------------------
 	-- Leatrix Sounds Sound File Listing
-	-- Community CSV for wow_classic_era 1.14.1.41030 (ClassicRetail)
+	-- Community CSV for wow_classic_era 1.14.1.41137 (ClassicRetail)
 	----------------------------------------------------------------------
 
 	local void, Leatrix_Sounds = ...
 	local L = Leatrix_Sounds.L
 
-	Leatrix_Sounds["ListFileVersion"] = "1.14.1.41030"
+	Leatrix_Sounds["ListFileVersion"] = "1.14.1.41137"
 
 	Leatrix_Sounds["MP3"] = {
 
