903cadd4bad076126091cea23c85fa8e9be53c95
leatrix <support@leatrix.com>
2024-02-12 11:38:04 +0000

    1.15.18.alpha.1

--- Leatrix_Sounds.lua
+++ Leatrix_Sounds.lua
@@ -373,7 +373,7 @@
 		scrollFrame = CreateFrame("ScrollFrame", "LeaSoundsScrollFrame", LeaSoundsLC["PageF"], "ScrollFrameTemplate")
 		scrollFrame:SetPoint("TOPLEFT", 0, -32)
 		scrollFrame:SetPoint("BOTTOMRIGHT", -30, 52)
-		scrollFrame:SetPanExtent(1)
+		scrollFrame:SetPanExtent(1.01)
 		scrollFrame:SetScript("OnVerticalScroll", UpdateList)
 
 		-- Create the scroll child
