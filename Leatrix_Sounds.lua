7d3edfbf54fe45457087ecd8febae52efc51fabe
leatrix <support@leatrix.com>
2024-11-22 10:45:10 +0000

    1.15.60g

--- Leatrix_Sounds.lua
+++ Leatrix_Sounds.lua
@@ -26,10 +26,12 @@
 
 
 
+local waitFrame = CreateFrame("FRAME")
+waitFrame:RegisterEvent("PLAYER_LOGIN")
+waitFrame:SetScript("OnEvent", function()
 
 
-
-
+C_Timer.After(2, function()
 
 	local function MakeTx(frame, title, x, y)
 		local text = frame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
@@ -51,7 +53,7 @@
 
 	local frame = CreateFrame("FRAME", nil, UIParent)
 
-	frame:SetSize(720, 220); frame:SetFrameStrata("FULLSCREEN_DIALOG"); frame:SetFrameLevel(100)
+	frame:SetSize(720, 266); frame:SetFrameStrata("FULLSCREEN_DIALOG"); frame:SetFrameLevel(100)
 	frame.tex = frame:CreateTexture(nil, "BACKGROUND"); frame.tex:SetAllPoints(); frame.tex:SetColorTexture(0.05, 0.05, 0.05, 0.9)
 	frame.close = CreateCloseButton(frame, 30, 30, "TOPRIGHT", 0, 0)
 	frame.close:SetScript("OnClick", function() frame:Hide() end)
@@ -65,12 +67,12 @@
 	MakeTx(frame, "- Find the Leatrix Sounds addon on CurseForge App, CurseForge website or leatrix.com.", 10, -130)
 	MakeTx(frame, "- Install Leatrix Sounds from there.", 10, -150)
 
-	MakeTx(frame, "The new project for all game versions is at curseforge.com/wow/addons/leatrix-sounds.", 10, -190)
+	MakeTx(frame, "The new project for all game versions is at curseforge.com/wow/addons/leatrix-sounds", 10, -190)
 
 	frame:SetPoint("CENTER", UIParent, "CENTER", 0, -768)
 
 	local startY, endX, endY, duration, elapsedTime = -768, 0, 0, 2.5, 0
-	PlaySoundFile("Interface\\AddOns\\Leatrix_Sounds\\sound.mp3", "MASTER")
+	PlaySoundFile("Interface\\AddOns\\Leatrix_Sounds\\sound2.mp3", "MASTER")
 
 	frame:SetScript("OnUpdate", function(self, elapsed)
 		elapsedTime = elapsedTime + elapsed
@@ -83,3 +85,45 @@
 			self:SetScript("OnUpdate", nil)
 		end
 	end)
+
+
+
+				local petEB = CreateFrame("EditBox", nil, frame)
+				petEB:SetPoint("TOPLEFT", frame, "BOTTOMLEFT", 10, 36)
+				petEB:SetTextInsets(2, 2, 2, 2)
+				petEB:SetFontObject("GameFontNormalLarge")
+				petEB:SetTextColor(1.0, 1.0, 1.0, 1)
+				petEB:SetBlinkSpeed(0)
+				petEB:SetAltArrowKeyMode(true)
+				petEB:SetFrameLevel(5000)
+				petEB:SetSize(600, 20)
+
+
+
+				local function RefreshPets()
+					petEB:SetText("https://www.curseforge.com/wow/addons/leatrix-sounds")
+					petEB:HighlightText()
+					petEB:SetFocus()
+				end
+
+				RefreshPets()
+
+				petEB:SetScript("OnEscapePressed", function() frame:Hide() end)
+				petEB:SetScript("OnEnterPressed", function() petEB:HighlightText() end)
+				petEB:SetScript("OnMouseDown", function() petEB:ClearFocus() end)
+				petEB:SetScript("OnMouseUp", function() petEB:HighlightText() end)
+				petEB:SetScript("OnChar", RefreshPets)
+				petEB:SetScript("OnKeyUp", RefreshPets)
+
+				petEB.tiptext = "Install Leatrix Sounds from here.  Press CTRL/C to copy."
+				petEB:SetScript("OnEnter", function()
+					RefreshPets()
+					GameTooltip:SetOwner(petEB, "ANCHOR_TOP")
+					GameTooltip:SetText(petEB.tiptext, nil, nil, nil, nil, true)
+				end)
+				petEB:HookScript("OnLeave", GameTooltip_Hide)
+
+
+end)
+
+end)
