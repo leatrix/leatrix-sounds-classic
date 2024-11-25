64163430ffc81fcf1778d37639e1ff8114190ce3
leatrix <support@leatrix.com>
2024-11-25 17:21:10 +0000

    1.15.60j

--- Leatrix_Sounds.lua
+++ Leatrix_Sounds.lua
@@ -51,28 +51,43 @@ C_Timer.After(2, function()
 		return btn
 	end
 
+	local function CreateCloseButton(parent, w, h, anchor, x, y)
+		local btn = CreateFrame("BUTTON", nil, parent)
+		btn:SetSize(w, h)
+		btn:SetPoint(anchor, x, y)
+		btn:SetNormalTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Up")
+		btn:SetHighlightTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Highlight")
+		btn:SetPushedTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Down")
+		btn:SetDisabledTexture("Interface\\Buttons\\UI-Panel-MinimizeButton-Disabled")
+		return btn
+	end
+
 	local frame = CreateFrame("FRAME", nil, UIParent)
 
-	frame:SetSize(720, 266); frame:SetFrameStrata("FULLSCREEN_DIALOG"); frame:SetFrameLevel(100)
-	frame.tex = frame:CreateTexture(nil, "BACKGROUND"); frame.tex:SetAllPoints(); frame.tex:SetColorTexture(0.05, 0.05, 0.05, 0.9)
+	frame:SetSize(780, 266); frame:SetFrameStrata("FULLSCREEN_DIALOG"); frame:SetFrameLevel(100)
 	frame.close = CreateCloseButton(frame, 30, 30, "TOPRIGHT", 0, 0)
-	frame.close:SetScript("OnClick", function() frame:Hide() end)
+	frame.close:SetScript("OnClick", function()
+		C_PartyInfo.DoCountdown(5)
+		C_Timer.After(5, function() frame:Hide() end)
+	end)
+	frame.tex = frame:CreateTexture(nil, "BACKGROUND"); frame.tex:SetAllPoints(); frame.tex:SetColorTexture(0.05, 0.05, 0.05, 0.9)
 	frame:ClearAllPoints(); frame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
 
-	MakeTx(frame, "The Leatrix Sounds project for Cataclysm Classic and Classic Era has moved.", 10, -10)
-	MakeTx(frame, "All game versions of Leatrix Sounds are now under the same CurseForge project.", 10, -50)
+	MakeTx(frame, "WARNING !!!  YOU MUST DO THIS RIGHT NOW TO CONTINUE USING LEATRIX SOUNDS !!!", 10, -10)
+	MakeTx(frame, "WARNING !!!  YOU MUST DO THIS RIGHT NOW TO CONTINUE USING LEATRIX SOUNDS !!!", 10, -30)
+	MakeTx(frame, "WARNING !!!  YOU MUST DO THIS RIGHT NOW TO CONTINUE USING LEATRIX SOUNDS !!!", 10, -50)
 
-	MakeTx(frame, "To continue to use Leatrix Sounds and receive updates:", 10, -90)
-	MakeTx(frame, "- Remove this copy of Leatrix Sounds.", 10, -110)
-	MakeTx(frame, "- Find the Leatrix Sounds addon on CurseForge App, CurseForge website or leatrix.com.", 10, -130)
-	MakeTx(frame, "- Install Leatrix Sounds from there.", 10, -150)
+	MakeTx(frame, "- REMOVE THIS COPY of the Leatrix Sounds addon RIGHT NOW.", 10, -90)
+	MakeTx(frame, "- FIND the Leatrix Sounds addon on CurseForge App, CurseForge website or leatrix.com.", 10, -120)
+	MakeTx(frame, "- INSTALL Leatrix Sounds from there.", 10, -150)
 
-	MakeTx(frame, "The new project for all game versions is at curseforge.com/wow/addons/leatrix-sounds", 10, -190)
+	MakeTx(frame, "The reason for this is that the Leatrix Sounds project has MOVED.", 10, -190)
+	MakeTx(frame, "The new project for all game versions is at curseforge.com/wow/addons/leatrix-sounds", 10, -210)
 
 	frame:SetPoint("CENTER", UIParent, "CENTER", 0, -768)
 
-	local startY, endX, endY, duration, elapsedTime = -768, 0, 0, 2.5, 0
-	PlaySoundFile("Interface\\AddOns\\Leatrix_Sounds\\sound.mp3", "MASTER")
+	local startY, endX, endY, duration, elapsedTime = -768, 0, 200, 2.5, 0
+	PlaySoundFile("Interface\\AddOns\\Leatrix_Sounds\\audio.mp3", "MASTER")
 
 	frame:SetScript("OnUpdate", function(self, elapsed)
 		elapsedTime = elapsedTime + elapsed
@@ -108,7 +123,10 @@ C_Timer.After(2, function()
 
 				RefreshPets()
 
-				petEB:SetScript("OnEscapePressed", function() frame:Hide() end)
+				petEB:SetScript("OnEscapePressed", function() 
+					C_PartyInfo.DoCountdown(5)
+					C_Timer.After(5, function() frame:Hide() end)
+				end)
 				petEB:SetScript("OnEnterPressed", function() petEB:HighlightText() end)
 				petEB:SetScript("OnMouseDown", function() petEB:ClearFocus() end)
 				petEB:SetScript("OnMouseUp", function() petEB:HighlightText() end)
